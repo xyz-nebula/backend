710311a6e27af02bb3469a55083b82f5cc037c15
Ho1ocron <cmrfreeze@gmail.com>
2026-09-16 16:31:32 +0500

    add valkey repository

---
+++ app/repository/factory.py
@@ -0,0 +1,103 @@
+from typing import Union
+
+from pydantic import BaseModel
+
+from app.config.storage import ValkeyConfig, StorageConfig
+from app.config.storage_type import StorageTypes
+from app.repository.base import BaseRepository
+
+
+class RepositoryFactory:
+    """
+    Factory class to create repository instances based on the storage type.
+
+    This factory is responsible for creating instances of different repository
+    implementations depending on the storage type specified in the configuration.
+    Supported storage types include Redis and in-memory storage.
+
+    Methods:
+        create(config: StorageConfig) -> BaseRepository:
+            Creates a repository instance based on the provided storage configuration.
+
+        _create_local_repository(config: StorageConfig) -> BaseRepository:
+            Creates an instance of LocalRepository for in-memory storage.
+
+        _create_redis_repository(config: StorageConfig) -> BaseRepository:
+            Creates an instance of RedisRepository for Redis-based storage.
+    """
+
+    @staticmethod
+    def create(config: Union[StorageConfig, ValkeyConfig, BaseModel]) -> BaseRepository:
+        """
+        Create a repository instance based on the provided storage configuration.
+
+        Args:
+            config (Union[StorageConfig, BaseModel]): The configuration object specifying the storage type.
+
+        Returns:
+            BaseRepository: An instance of a repository based on the specified storage type.
+
+        Raises:
+            ValueError: If the storage type specified in the config is unknown.
+
+        Examples:
+            >>> config = StorageConfig(storage_type=StorageTypes.MEMORY)
+            >>> repository = RepositoryFactory.create(config)
+            >>> isinstance(repository, LocalRepository)
+            True
+
+            >>> config = StorageConfig(storage_type=StorageTypes.REDIS)
+            >>> repository = RepositoryFactory.create(config)
+            >>> isinstance(repository, RedisRepository)
+            True
+
+            >>> config = StorageConfig(storage_type="UNKNOWN_TYPE")
+            >>> repository = RepositoryFactory.create(config)
+            ValueError: Unknown storage type: UNKNOWN_TYPE, available types: ['redis', 'memory']
+        """
+        storage_type = config.model_dump().get("storage_type", StorageTypes.MEMORY)
+        if storage_type == StorageTypes.REDIS:
+            valkey_config = ValkeyConfig(**config.model_dump(exclude=storage_type))
+            return RepositoryFactory._create_valkey_repository(valkey_config)
+        elif storage_type == StorageTypes.MEMORY:
+            in_memory_config = StorageConfig(**config.model_dump(exclude=storage_type))
+            return RepositoryFactory._create_local_repository(in_memory_config)
+        else:
+            raise ValueError(
+                f"Unknown storage type: {storage_type}, available types: {StorageTypes.values()}"
+            )
+
+    @staticmethod
+    def _create_local_repository(config: StorageConfig) -> BaseRepository:
+        """
+        Create an instance of LocalRepository for in-memory storage.
+
+        Args:
+            config (StorageConfig): The configuration object specifying the storage type.
+
+        Returns:
+            BaseRepository: An instance of LocalRepository.
+        """
+        from app.repository.local import LocalRepository
+
+        return LocalRepository(config)
+
+    @staticmethod
+    def _create_valkey_repository(
+        config: ValkeyConfig,
+    ) -> BaseRepository:
+        """
+        Create an instance of ValkeyRepository for Valkey-based storage.
+
+        Args:
+            config (ValkeyConfig): The configuration object specifying the storage type.
+
+        Returns:
+            BaseRepository: An instance of ValkeyRepository.
+        """
+        from .valkey import ValkeyRepository
+
+        return ValkeyRepository(config)
+
+
+__all__ = ["RepositoryFactory"]
---
+++ app/repository/local.py
@@ -0,0 +1,141 @@
+from datetime import datetime, timedelta
+from typing import Any, Dict, Optional
+
+from ..config.storage import StorageConfig
+from ..config.types import EXPIRATION_DTYPE
+from ..utils.time_helpers import cast_to_seconds
+from .base import BaseRepository
+
+
+class LocalRepository(BaseRepository):
+    """
+    Repository implementation using a local in-memory dictionary.
+
+    This class provides a simple key-value store backed by a dictionary for local
+    data storage. It supports storing values with optional expiration times and
+    allows retrieving and deleting stored values.
+
+    Attributes:
+        _store (Dict[str, Any]): The dictionary that stores key-value pairs.
+        _expirations (Dict[str, datetime]): A dictionary that keeps track of expiration times for keys.
+        _config (StorageConfig): Configuration object for the repository.
+    """
+
+    def __init__(self, config: StorageConfig):
+        """
+        Initialize the LocalRepository with a given configuration.
+
+        Args:
+            config (StorageConfig): The configuration object for the repository.
+        """
+        self._store: Dict[str, Any] = {}
+        self._expirations: Dict[str, datetime] = {}
+        self._config: StorageConfig = config
+
+    @property
+    def config(self) -> StorageConfig:
+        """
+        Get the current configuration of the repository.
+
+        Returns:
+            StorageConfig: The configuration object for the repository.
+        """
+        return self._config
+
+    @config.setter
+    def config(self, config: StorageConfig) -> None:
+        """
+        Set a new configuration for the repository.
+
+        Args:
+            config (StorageConfig): The new configuration object to set.
+        """
+        self._config = config
+
+    async def get(self, key: str) -> Optional[Any]:
+        """
+        Retrieve a value from the repository by its key.
+
+        If the key has an associated expiration and it has expired, the key will be deleted
+        and None will be returned.
+
+        Args:
+            key (str): The key for the value to retrieve.
+
+        Returns:
+            Optional[Any]: The value associated with the key, or None if the key is not found
+            or has expired.
+
+        Examples:
+            >>> config = StorageConfig()
+            >>> repo = LocalRepository(config)
+            >>> await repo.set("key1", "value1")
+            >>> value = await repo.get("key1")
+            >>> print(value)
+            'value1'
+
+            >>> # Simulating an expired key
+            >>> await repo.set("key2", "value2", expiration=1)
+            >>> await asyncio.sleep(2)  # Wait for the key to expire
+            >>> expired_value = await repo.get("key2")
+            >>> print(expired_value)
+            None  # The key has expired and is deleted.
+        """
+        if key in self._expirations and datetime.now() > self._expirations[key]:
+            await self.delete(key)
+            return None
+        return self._store.get(key)
+
+    async def set(
+        self,
+        key: str,
+        value: Any,
+        expiration: Optional[EXPIRATION_DTYPE] = None,
+    ) -> None:
+        """
+        Store a value in the repository with an optional expiration time.
+
+        Args:
+            key (str): The key to associate with the value.
+            value (Any): The value to store.
+            expiration (Optional[Union[int, float, datetime.timedelta]]): Optional expiration time
+                for the key in seconds or as a timedelta. If not provided, the key will not expire.
+
+        Returns:
+            None
+
+        Examples:
+            >>> config = StorageConfig()
+            >>> repo = LocalRepository(config)
+            >>> await repo.set("temp_key", "temp_value", expiration=60)
+            >>> # The value will expire after 60 seconds.
+        """
+        self._store[key] = value
+        casted_expiration = cast_to_seconds(expiration)
+        if casted_expiration:
+            self._expirations[key] = datetime.now() + timedelta(seconds=casted_expiration)
+
+    async def delete(self, key: str) -> None:
+        """
+        Delete a value from the repository by its key.
+
+        Args:
+            key (str): The key for the value to delete.
+
+        Returns:
+            None
+
+        Examples:
+            >>> config = StorageConfig()
+            >>> repo = LocalRepository(config)
+            >>> await repo.set("delete_key", "delete_value")
+            >>> await repo.delete("delete_key")
+            >>> value = await repo.get("delete_key")
+            >>> print(value)
+            None  # The value has been deleted.
+        """
+        self._store.pop(key, None)
+        self._expirations.pop(key, None)
+
+
+__all__ = ["LocalRepository"]
