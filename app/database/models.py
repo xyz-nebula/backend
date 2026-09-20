b693ec9928ae2f9ea299971ef2729303552ce35f
Ho1ocron <cmrfreeze@gmail.com>
2026-09-20 22:37:13 +0500

    add judgemnt and feedback models

--- app/database/models.py
+++ app/database/models.py
@@ -5,6 +5,23 @@ from tortoise import fields
 from tortoise.models import Model
 
 
+"""
+Feedback
+- id (INT, PK)
+- uuid (UUID, UK)
+- created_at (DATETIME)
+- session_uuid (UUID, FK)
+- text (TEXT)
+
+Judgement
+- id (INT, PK)
+- uuid (UUID, UK)
+- created_at (DATETIME)
+- session_uuid (UUID, FK)
+- text (TEXT)
+"""
+
+
 class UserStatus(StrEnum):
     PENDING_ACTIVATION = "pending_activation"
     ACTIVE = "active"
@@ -53,6 +70,36 @@ class Chat(Model):
 
     def __str__(self) -> str:
         return f"Chat({self.uuid}, {self.name})"
+
+
+class Feedback(Model):
+    id = fields.IntField(pk=True)
+    uuid = fields.UUIDField(unique=True, default=uuid.uuid4)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    session_uuid = fields.UUIDField()
+    text = fields.TextField()
+
+    class Meta:
+        table = "feedback"
+
+    def __str__(self) -> str:
+        return f"Feedback({self.uuid}, {self.text[:20]}...)"
+
+
+class Judgement(Model):
+    id = fields.IntField(pk=True)
+    uuid = fields.UUIDField(unique=True, default=uuid.uuid4)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    session_uuid = fields.UUIDField()
+    text = fields.TextField()
+
+    class Meta:
+        table = "judgement"
+
+    def __str__(self) -> str:
+        return f"Judgement({self.uuid}, {self.text[:20]}...)"
 
 
 class Message(Model):
